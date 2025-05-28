93c32b8819375ece177514ff92f0068bf525d15d
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2025-05-27 22:40:48 -0700

    feat: add asteroid system
    - Add Asteroid and AsteroidField classes
    - Create dedicated sprite group for asteroids
    - Set up asteroid and asteroid field containers
    - Initialize asteroid field in main game loop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -11,14 +13,20 @@ def main():
     # groups for updating and drawing
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
 
-    # player
+    # player and asteroids
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
+
 
     while 1:
         # event handling
