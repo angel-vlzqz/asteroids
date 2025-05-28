90bcd0da689ce3844dff4e9e94deb29ebfd50533
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2025-05-27 21:44:41 -0700

    refactor: implement sprite groups for game object management
    - Add updatable and drawable sprite groups
    - Move player into sprite groups
    - Refactor update and draw logic to use sprite groups

--- main.py
+++ main.py
@@ -8,20 +8,30 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
 
+    # groups for updating and drawing
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
 
+    # player
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while 1:
+        # event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
         dt = clock.tick(60) / 1000
-        player.update(dt)
-        player.draw(screen)
+
+        # update and draw
+        updatable.update(dt)
+        for drawing in drawable:
+            drawing.draw(screen)
         pygame.display.flip()
 
 if __name__ == '__main__':
